4b2a13c030db44fbc6a29be31c30725f85bf44ad
Nick <KlimNikAlex@yandex.ru>
2024-02-27 20:56:14 +0300

    I finished api.py

--- api.py
+++ api.py
@@ -4,28 +4,31 @@ app = Flask(__name__)
 
 import model
 
+class ApiException(Exception):
+    pass
+
 API_ROOT = "/api/v1"
 NOTE_API_ROOT = API_ROOT + "/note"
 
 @app.route(NOTE_API_ROOT + "/", methods=["POST"])
 def create():
-    pass
+    return "create", 201
 
 @app.route(NOTE_API_ROOT + "/", methods=["GET"])
 def list():
-    pass
+    return "list", 201
 
-@app.route(NOTE_API_ROOT + "<_id>", methods=["GET"])
-def reade():
-    pass
+@app.route(NOTE_API_ROOT + "/<_id>/", methods=["GET"])
+def reade(_id: str):
+    return "reade", 201
 
-@app.route(NOTE_API_ROOT + "<_id>", methods=["PUT"])
-def update():
-    pass
+@app.route(NOTE_API_ROOT + "/<_id>/", methods=["PUT"])
+def update(_id: str):
+    return "update", 201
 
-@app.route(NOTE_API_ROOT + "<_id>", methods=["DELET"])
-def delete():
-    pass
+@app.route(NOTE_API_ROOT + "/<_id>/", methods=["DELET"])
+def delete(_id: str):
+    return "delete", 201
 
 if __name__ == '__main__':
     app.run(debug=True)
